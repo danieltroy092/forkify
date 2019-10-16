fd2dbe983c9da1343250ca6d1cfbf94144a48bec
danieltroy092 <danieltroy092@gmail.com>
2019-10-16 16:32:33 +0100

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,2 @@
+export const proxy = 'https://cors-anywhere.herokuapp.com/';
+export const key = 'f31b78b65b6ee4fb8d8b75b559dfb279';
---
+++ src/js/index.js
@@ -0,0 +1,86 @@
+import Search from './models/Search';
+import Recipe from './models/Recipe';
+import * as searchView from './views/searchView';
+import { elements, renderLoader, clearLoader } from './views/base';
+
+/** Global state of the app
+ * - Search Object
+ * - Current recipe object
+ * - Shopping list object
+ * - liked recipes
+ */
+const state = {};
+
+/** SEARCH CONTROLLER **/
+const controlSearch = async () => {
+  // 1) get query from view
+  const query = searchView.getInput();
+
+  if (query) {
+    // 2) New search obj and add to state
+    state.search = new Search(query);
+
+    // 3) Prepare UI for results
+    searchView.clearInput();
+    searchView.clearResults();
+    renderLoader(elements.searchResults);
+
+    try {
+      // 4) Search for recipes
+      await state.search.getResults();
+
+      // 5) render results on UI
+      clearLoader();
+      searchView.renderResults(state.search.result);
+    } catch (error) {
+      alert('Something wrong with search...');
+      clearLoader();
+    }
+  }
+};
+
+elements.searchForm.addEventListener('submit', e => {
+  e.preventDefault();
+  controlSearch();
+});
+
+elements.searchResPages.addEventListener('click', e => {
+  const btn = e.target.closest('.btn-inline');
+  if (btn) {
+    const goToPage = parseInt(btn.dataset.goto, 10);
+    searchView.clearResults();
+    searchView.renderResults(state.search.result, goToPage);
+  }
+});
+
+/** RECIPE CONTROLLER **/
+const controlRecipe = async () => {
+  const id = window.location.hash.replace('#', '');
+  console.log(id);
+
+  if (id) {
+    //Prepare for UI changes
+
+    //Create new recipe object
+    state.recipe = new Recipe(id);
+
+    try {
+      //get recipe data and parse ingredients
+      await state.recipe.getRecipe();
+      state.recipe.parseIngredients();
+
+      //calculate servings and time
+      state.recipe.calcTime();
+      state.recipe.calcServings();
+
+      //render recipe
+      console.log(state.recipe);
+    } catch (error) {
+      alert('Error processing recipe');
+    }
+  }
+};
+
+['hashchange', 'load'].forEach(event =>
+  window.addEventListener(event, controlRecipe)
+);
